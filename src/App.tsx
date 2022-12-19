04c54664cf83bcd96561de672a0171eb6b7cb57e
Hamid Asadi <hamid.19471@gmail.com>
2022-12-19 22:58:19 +0330

    add router and outlet layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,48 @@
 import React from "react";
-import { createBrowserRouter, Outlet } from "react-router-dom";
-import tw from "twin.macro";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+import Footer from "./Components/Footer/Footer";
+import Header from "./Components/Header/Header";
+import Category from "./Pages/Category/Category";
+import Home from "./Pages/Home/Home";
+import Product from "./Pages/Product/Product";
 
-const Container = tw.div`
-w-full
-bg-indigo-700
-p-20
-`;
+const Layout = () => {
+    return (
+        <div>
+            <Header />
+            <Outlet />
+            <Footer />
+        </div>
+    );
+};
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Layout />,
+        children: [
+            {
+                path: "/",
+                element: <Home />,
+            },
+            {
+                path: "/category/:id",
+                element: <Category />,
+            },
+            {
+                path: "product/:id",
+                element: <Product />,
+            },
+        ],
+    },
+]);
 
 function App() {
-    return <Container>Hamidkhan</Container>;
+    return (
+        <>
+            <RouterProvider router={router} />
+        </>
+    );
 }
 
 export default App;
